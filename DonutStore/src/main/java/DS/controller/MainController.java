ecea66800f350142591cbefa05ea66f0a9a2b19c
Hoan <hoan@Hoans-MacBook-Air.local>
2017-10-30 07:17:53 +0700

    7:17.AM 30/10/2017

--- DonutStore/src/main/java/DS/controller/MainController.java
+++ DonutStore/src/main/java/DS/controller/MainController.java
@@ -7,13 +7,24 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String index() {
+    public String home(Model model,Authentication auth, RedirectAttributes redirect) {
+    	auth = SecurityContextHolder.getContext().getAuthentication();
+		boolean userRole0 = auth.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("ROLE_MEMBER"));
+		if(userRole0) {
+			model.addAttribute("logined", "You have been logined");
+		}
+		boolean userRole1 = auth.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("ROLE_ADMIN"));
+		if(userRole1) {
+			model.addAttribute("loginedAdmin", "You have been logined as ADMIN");
+		}
         return "home";
     }
     
