acf50597ec2931c10ef1d9d987f2bd72eb4d3503
DESKTOP-5QLLELC\HitKeoDog <hoanporsche@gmail.com>
2018-08-06 11:07:57 +0700

    build react app

--- react-donut-store copy/src/share/constant/configuration.constant.ts
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-    SEARCH_MAX_LENGTH: 50,
-    REQUEST_TITLE_MAX_LENGTH: 50,
-    PAGE_SIZE: 15,
-    PAGE_MAX_SIZE: 2000,
-};
--- react-donut-store copy/src/share/constant/role.constant.ts
+++
@@ -1,6 +0,0 @@
-
-export const ROLES = Object.freeze({
-    ADMIN: 'ROLE_ADMIN',
-    STORE: 'ROLE_STORE',
-    STAFF: 'ROLE_STAFF'
-});
