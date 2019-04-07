f5ff539643631f8fc5b4ca1fa5b28fae607efadd
Hoan <hoanporsche@gmail.com>
2019-04-07 07:14:42 +0700

    sdds

--- DonutStoreUpgrade/src/main/java/ds/upgrade/controller/HomeController.java
+++ DonutStoreUpgrade/src/main/java/ds/upgrade/controller/HomeController.java
@@ -1,15 +1,22 @@
 package ds.upgrade.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ds.upgrade.service.CategoryService;
+
 @Controller
 public class HomeController {
+  
+  @Autowired
+  private CategoryService categoryService;
 
   @GetMapping("/")
   public String webSocket(Model model) {
     model.addAttribute("title","Banh ran Vu Hoan");
-    return "home";
+    model.addAttribute("categories",categoryService.findAllJson());
+    return "public/home";
   }
 }
